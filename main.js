0d590ca1db2ffee342075e3b8b44f65d4c3d4f39
agrah355 <74407072+agrah355@users.noreply.github.com>
2021-04-18 18:25:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preLoad(){
+
+}
+
+function setup(){
+    canvas=createCanvas(700,500);
+    canvas.position(110,250);
+}
+function draw(){
+    fill(225, 115, 1, 200);
+    stroke(100, 100, 120);
+    ellipse(30,30,50,50);
+    rect(40,10,620,20);
+    ellipse(670,470,50,50);
+    rect(40,470,620,20);
+    ellipse(30,470,50,50);
+    rect(10,40,20,420)
+    ellipse(670,30,50,50);
+    rect(670,40,20,420)
+}
